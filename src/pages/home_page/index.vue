e0cf44ffdd6c045fb01df718ccd9ec205bf1a013
C Z <cz@CdeMacBook-Pro.local>
2021-08-08 19:09:26 -0700

    fix: bug

--- src/pages/home_page/index.vue
+++ src/pages/home_page/index.vue
@@ -18,15 +18,15 @@
     <div class="sizedbox-height-80"></div>
     <div class="olpcgaf-homepage-card">
        <div class="olpcgaf-homepage-card-title">OLPC总供应量</div>
-       <div class="olpcgaf-homepage-card-price">100,000,000 </div>
+       <div class="olpcgaf-homepage-card-price">{{maxSupply}}</div>
        <div class="olpcgaf-homepage-card-footer">
          <span>最大总供应量</span>
-         <span>100,000,000  OLPC</span>
+         <span>{{maxSupply}} OLPC</span>
         </div>
     </div>
     <div class="olpcgaf-homepage-card">
        <div class="olpcgaf-homepage-card-title">我的OLPC余额</div>
-       <div class="olpcgaf-homepage-card-price">999</div>
+       <div class="olpcgaf-homepage-card-price">{{rewardUserIncome}}</div>
        <div class="olpcgaf-homepage-card-footer">仅有60天的挖矿时间，马上开始挖吧</div>
     </div>
     <div class="olpcgaf-homepage-darkcard">
@@ -34,21 +34,21 @@
           <img src="../../assets/LOGO@3x.png" class="olpcgaf-homepage-darkcard-logo" alt="">
           <div class="olpcgaf-homepage-darkcard-content">
             <div class="olpcgaf-homepage-text text-left">我们为您提供10%的OLPC推荐奖励。复制下面的推荐链接立即分享并获得奖励。</div>
-            <input class="recomend-link" placeholder="请输入钱包地址" id="copy"/>
-            <div class="olpcgaf-homepage-darkcard-btn" data-clipboard-target="#copy">复制</div>
+            <input class="recomend-link" placeholder="请输入钱包地址" v-model="code" id="copy"/>
+            <div class="olpcgaf-homepage-darkcard-btn" data-clipboard-target="#copy">复制并生成推荐关系</div>
           </div>
        </div>
        <div class="olpcgaf-homepage-darkcard-footer">
          <span>最大总供应量</span>
-         <span>100,000,000   OLPC</span>
+         <span>{{maxSupply}}  OLPC</span>
        </div>
     </div>
     <div class="olpcgaf-homepage-shadowcard">
        <img src="../../assets/gary_logo.png" class="olpcgaf-homepage-shadowcard-logo" alt="">
        <div class="olpcgaf-homepage-shadowcard-title">OLPC-TRX LP</div>
        <p class="olpcgaf-homepage-text">质押OLPC-TRX LP</p>
-       <p class="olpcgaf-homepage-text">赚取10000000 OLPC</p>
-       <p class="olpcgaf-homepage-link">查看合约</p>
+       <p class="olpcgaf-homepage-text">赚取{{maxSupply}} OLPC</p>
+       <p class="olpcgaf-homepage-link" @click="onCheckToken">查看合约</p>
        <p class="olpcgaf-homepage-shadowcard-text">{{allIcomeAmount}} OLPC准备收获</p>
        <div class="olpcgaf-homepage-shadowcard-btn" @click="onRouter(routerPaths.select_page)">选择</div>
     </div>
@@ -69,13 +69,13 @@
 <script lang='ts'>
 import { useRouter } from 'vue-router';
 import Decimal from 'decimal.js';
-import { Toast } from 'vant';
+import { Toast, Dialog } from 'vant';
 import ClipboardJS from 'clipboard';
 import { pow } from '@/constants/index';
 import * as utils from '@/utils/index';
 import { useGlobalHooks } from '@/hooks';
 import * as routerPaths from '@/constants/app_routes_path';
-import { tokenContract, SinglePie, MultiPie } from '@/tronlink/index';
+import {  SinglePie, MultiPie, RewardContract, OlptContract, pieAddress } from '@/tronlink/index';
 import { onMounted,ref, computed } from 'vue';
 export default {
     name: 'home_page',
@@ -83,9 +83,13 @@ export default {
       const { wellet_address, onGetWellet } = useGlobalHooks();
       const singleUserIncome = ref<number>(0);  // 单币矿池收益
       const doubleUserIncome = ref<number>(0);  // 双币矿池收益
+      const rewardUserIncome = ref<number>(0);  // 推荐收益
+      const maxSupply = ref<number>(0); // 最大供应量
+      const code = ref<string>(''); // 推荐人钱包地址
       const router = useRouter();
-      const contract = new tokenContract();
       const singlePie = new SinglePie();
+      const rewardContract = new RewardContract();
+      const olptContract = new OlptContract();
       const multiPie = new MultiPie();
       const onRouter = (path: string) => {
         router.push(path)
@@ -117,6 +121,50 @@ export default {
            utils.loadingClean();
           }
       }
+      /** 获取用最大供应量 */
+      const onGetMaxSupply = async () => {
+          try {
+            const res = await olptContract.maxSupply();
+            const supply = (window as any).tronWeb.toDecimal(res);
+            maxSupply.value = Number(new Decimal(supply).div(pow))
+          } catch(err) {
+           utils.toast(err || err.message);
+           utils.loadingClean();
+          }
+      }
+
+      /** 获取通过推荐获得的收益 */
+      const onGetRewardUserIncome = async () => {
+          try {
+            const res = await rewardContract.userIncome();
+            const income = (window as any).tronWeb.toDecimal(res.income);
+            rewardUserIncome.value = Number(new Decimal(income).div(pow))
+          } catch(err) {
+           utils.toast(err || err.message);
+           utils.loadingClean();
+          }
+      }
+
+      /** 生成推荐关系 */
+      const onRecommedCode = async () => {
+          try {
+            utils.loading('正在生成推荐关系')
+            await rewardContract.recommedCode(code.value);
+            Toast.success('复制成功');
+          } catch(err) {
+           utils.toast(err || err.message);
+           utils.loadingClean();
+          }
+      }
+
+      const onCheckToken = () => {
+        Dialog.alert({
+          title: '合约地址',
+          message: pieAddress.single + ',' + pieAddress.multi,
+        }).then(() => {
+          // on close
+        });
+      }
 
       const allIcomeAmount = computed({
             get: () => {
@@ -133,16 +181,18 @@ export default {
       onMounted(() => {
         onGetUserIncome();
         onGetDoubleUserIncome();
-        let ClipboardJSObj=new ClipboardJS('.olpcgaf-homepage-darkcard-btn')
+        onGetMaxSupply();
+        onGetRewardUserIncome();
+        let ClipboardJSObj= new ClipboardJS('.olpcgaf-homepage-darkcard-btn')
             ClipboardJSObj.on('success', function(e) {
-                Toast.success('复制成功')
-                e.clearSelection();
+              onRecommedCode();
+              e.clearSelection();
             });
             ClipboardJSObj.on('error', function(e) {
                 e.clearSelection();
             })
       })
-      return {routerPaths,wellet_address, onRouter, onGetWellet, onOpenWemmet, allIcomeAmount} 
+      return {routerPaths,wellet_address,maxSupply , rewardUserIncome,code, onRouter,onCheckToken,  onGetWellet, onOpenWemmet, allIcomeAmount} 
     }
   };
 </script>
--- src/tronlink/index.js
+++ src/tronlink/index.js
@@ -1,18 +1,27 @@
 const FarmOneAsset = require('./FarmOneAsset.json');
 const FarmTwoAsset = require('./FarmTwoAsset.json');
 const ITRC20 = require('./ITRC20.json');
+const Recommed = require('./Recommed.json');
+const Olpc = require('./Olpc.json');
 
 /** 代币地址 */
+// export const tokenAddress = {
+//     OLPC: 'TA15uPAzkwbXUtzK8cUvrLKUjcjiEY99RS',
+//     GAFP: 'TVbRqw2oZTyD8sPojc9Gjb4q5aGR125hgx',
+//     OLPP: 'TANvziYPGfYkY1nfuXgH2tTTq4ZmzZ8mcT'
+// }
 export const tokenAddress = {
     OLPC: 'TA15uPAzkwbXUtzK8cUvrLKUjcjiEY99RS',
-    GAFP: 'TVbRqw2oZTyD8sPojc9Gjb4q5aGR125hgx',
-    OLPP: 'TANvziYPGfYkY1nfuXgH2tTTq4ZmzZ8mcT'
+    GAFP: 'TVpN8X4FUAsDAW6J9m3c58v3dacJJgpH8N',
+    OLPP: 'TYuhhCibRSuw3mECXcLbKrLK9XnubdAX1y'
 }
+
 
 /** 矿池地址 */
 export const pieAddress = {
-    single: 'TLn8Ekq1tVShsqmp33mGbNfhx73WA7nSZR',
-    multi: 'TCDr3xKF28LZ7M7k8sXStkmRQBd5QxHWLx'
+    single: 'TJnaM32oyo3PqkCUpQ2vGg1qZ2BnYrPfWG',
+    multi: 'TAa3Gw2rAPnfN4czsWcGRNKFDjPsmdKoJW',
+    reward: 'TTCDSKBXz44xiWfJvBYDTFX8CvkrDLtpy2'
 }
 
 /** 
@@ -37,15 +46,34 @@ export const tronWebApprove = (token, address) => {
     })
 }
 
-/** 初始化代币实例 */
-export const initTokenContract = () => {
+/** 初始化OLPT代币实例, 用来获取最大供应量 */
+export const initOlptContract = () => {
     return new Promise((resolve, reject) => {
         (async() => {
             try {
                 /** 授权数量 直接授权å一个亿 第二次投资的时候 可以查询授权数量还有 就不用再次授权 */
                 const amount = 100000000 * Math.pow(10, 18);
-                const contract = await window.tronWeb.contract(ITRC20.abi, tokenAddress.GAFP);
-                window.contract = contract;
+                const contract = await window.tronWeb.contract(Olpc.abi, 'TLggZp2ida3LgTuX3FdWKTXBp2dCzeb4Lt');
+                window.olptContract = contract;
+                resolve(contract);
+            } catch (err) {
+                console.log(err);
+                reject(err);
+            }
+        })();
+    })
+}
+
+/** reward矿池实例 */
+export const initRewardContract = () => {
+    return new Promise((resolve, reject) => {
+        (async() => {
+            try {
+                /** 授权数量 直接授权å一个亿 第二次投资的时候 可以查询授权数量还有 就不用再次授权 */
+                const amount = 100000000 * Math.pow(10, 18);
+                const contract = await window.tronWeb.contract(Recommed.abi, pieAddress.reward);
+                window.rewardContract = contract;
+                console.log(contract);
                 resolve(contract);
             } catch (err) {
                 console.log(err);
@@ -56,7 +84,6 @@ export const initTokenContract = () => {
 }
 
 
-
 /**
  * 初始化矿池实例
  */
@@ -64,8 +91,8 @@ export const initContract = () => {
     return new Promise((resolve, reject) => {
         (async() => {
             try {
-                const singlePieContractPromise = window.tronWeb.contract(FarmOneAsset.abi).at(pieAddress.single);
-                const multiContractPromise = window.tronWeb.contract(FarmTwoAsset.abi).at(pieAddress.multi);
+                const singlePieContractPromise = window.tronWeb.contract(FarmOneAsset.abi, pieAddress.single);
+                const multiContractPromise = window.tronWeb.contract(FarmTwoAsset.abi, pieAddress.multi);
                 const [singlePieContract, multiPieContract] = await Promise.all([singlePieContractPromise, multiContractPromise]);
                 window.singlePieContract = singlePieContract;
                 window.multiPieContract = multiPieContract;
@@ -78,10 +105,35 @@ export const initContract = () => {
     })
 }
 
-export class tokenContract {
-    /** 获取总供应量 */
-    totalSupply() {
-        return window.contract.totalSupply();
+export class RewardContract {
+    /** 用户钱包地址 */
+    wellet_address = window.tronWeb.defaultAddress.base58
+
+    /** 生成推荐关系 */
+    recommedCode(_recommed) {
+            return window.rewardContract.recommedCode(_recommed).send();
+        }
+        /** 获取用户通过推荐获得的收益 */
+    userIncome() {
+            return window.rewardContract.userIncome(this.wellet_address).call();
+        }
+        /** 用户提取推荐收益 */
+    claim(amount) {
+            return window.rewardContract.claim(this.wellet_address, amount).send();
+        }
+        /** 获取用户的各项信息，1.质押数量（不用展示） 2.推荐算力 3.推荐人地址 4.用户负债（不用展示）5.用户收益（不用展示） */
+    getUserInfo(amount) {
+        return window.rewardContract.getUserInfo(this.wellet_address).call();
+    }
+}
+
+export class OlptContract {
+    /** 用户钱包地址 */
+    wellet_address = window.tronWeb.defaultAddress.base58
+
+    /** 用来获取最大供应量 */
+    maxSupply(_recommed) {
+        return window.olptContract.maxSupply().call();
     }
 }
 
